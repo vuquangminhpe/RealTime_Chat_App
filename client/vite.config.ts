5453efc3eaada4b9255f9a59d23433fdb6e00d3e
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-19 08:41:39 +0700

    Done setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,14 +6,14 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3002
   },
   css: {
     devSourcemap: true
   },
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
