8cc426cfddabca114f3323be9c13156a63e8b18c
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-28 22:35:04 +0700

    update Story routes

--- server/src/routes/storys.routes.ts
+++ server/src/routes/storys.routes.ts
@@ -1,3 +1,15 @@
 import { Router } from 'express'
+import { accessTokenValidator } from '~/middlewares/user.middlewares'
+import { wrapAsync } from '~/utils/handler'
 
 export const stories = Router()
+
+/**
+ * Description: Add to news
+ * Path: /add_to_news
+ * method: post
+ * headers: {access_token:string}
+ * body: {image: string}
+ */
+
+stories.post('/add_to_news', accessTokenValidator, wrapAsync(addToNewsController))
